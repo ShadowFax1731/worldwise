4ffebd76685fd605074139a6e4301c97d0e2ccf8
shadowfax <55938330+ShadowFax1731@users.noreply.github.com>
2024-12-05 13:21:53 +0530

    Building app layout (map and sidebar)

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,7 +1,13 @@
 import styles from "./AppLayout.module.css";
-
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 function AppLayout() {
-  return <div className={styles.app}>App</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout;
