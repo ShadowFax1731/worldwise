9b2c89329ece69f05b6cc44bd5587203773215d6
shadowfax <55938330+ShadowFax1731@users.noreply.github.com>
2024-12-06 15:31:47 +0530

    programmatic navigation with useNavigate()

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const URL = `http://localhost:8000`;
 
@@ -54,7 +55,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} loading={loading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
