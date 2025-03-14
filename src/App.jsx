e26ca7d4221f96b901fb49688aec1d8a3e536954
ShadowFax <55938330+ShadowFax1731@users.noreply.github.com>
2025-03-15 04:33:59 +0530

    Protecting a route(Unsaved App.jsx now saved)

--- src/App.jsx
+++ src/App.jsx
@@ -14,30 +14,35 @@ import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
-    return (
-            <AuthProvider>
-                <CitiesProvider>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<Homepage />} />
-                            <Route path="product" element={<Product />} />
-                            <Route path="pricing" element={<Pricing />} />
-                            <Route path="login" element={<Login />} />
-                            <Route path="app" element={
-                                <ProtectedRoute><AppLayout /></ProtectedRoute>
-                            } >
-                            <Route index element={<Navigate to="cities" replace />} />
-                            <Route path="cities" element={<CityList />} />
-                            <Route path="cities/:id" element={<City />} />
-                            <Route path="countries" element={<CountryList />} />
-                            <Route path="form" element={<Form />} />
-                        </Route>
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
-                </BrowserRouter>
-            </CitiesProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
